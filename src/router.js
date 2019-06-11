5541f86502aba80d2ab1b81e84e7a9f7c04f37c2
licy <licy@briup.com>
2019-06-11 16:29:54 +0800

    添加了订单项页面

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import OrderLinePage from './routes/OrderLinePage'
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/orderLine" exact component={OrderLinePage} />
       </Switch>
     </Router>
   );
