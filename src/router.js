a880db43675881be74c3477e414d2d04187c5996
licy <licy@briup.com>
2019-06-12 10:04:15 +0800

    顾客管理的添加功能

--- src/router.js
+++ src/router.js
@@ -7,14 +7,15 @@ import OrderPage from './routes/OrderPage'
 
 function RouterConfig({ history }) {
   return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={IndexPage} />
-        <Route path="/orderLine" exact component={OrderLinePage} />
-        <Route path="/customer" exact component={CustomerPage} />
-        <Route path="/order" exact component={OrderPage} />
-      </Switch>
-    </Router>
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={IndexPage} />
+          <Route path="/orderLine" exact component={OrderLinePage} />
+          <Route path="/customer" exact component={CustomerPage} />
+          <Route path="/order" exact component={OrderPage} />
+        </Switch>
+      </Router>
+    
   );
 }
 
